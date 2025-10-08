cda7aee8db700b2ead0d5a0a7a1ae54330001eb5
Ashmit Sharma <alpha5sector@gmail.com>
2025-10-08 15:45:31 +0530

    fix

--- server.js
+++ server.js
@@ -82,3 +82,4 @@ app.post('/send-email', upload.single('resume'), async (req, res) => {
 // -----------------------
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`📡 Server running on port ${PORT}`));
+// -----------------------
